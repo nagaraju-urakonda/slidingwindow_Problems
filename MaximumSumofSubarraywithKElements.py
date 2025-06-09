1c5e14c7085eb33e26414cbf0bc1e408d8c91219
nagaraju-urakonda <nagarajuurakonda78@gmail.com>
2025-06-09 17:01:07 +0530

    Add files via upload

---
+++ MaximumSumofSubarraywithKElements.py
@@ -0,0 +1,16 @@
+#Maximum Sum of a Subarray with K Elements
+def s_w(arr,k):
+    n = len(arr)
+    cur_sum = sum(arr[:k])
+    max_sum = cur_sum
+    
+    for i in range(k,n):
+        cur_sum = cur_sum + arr[i] - arr[i-k]
+        max_sum = max(max_sum,cur_sum)
+        
+    return max_sum
+arr = [100,200,300,400,500]
+k = 2
+print(s_w(arr,k))
+        
+    
